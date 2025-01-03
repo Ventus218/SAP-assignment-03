0bc0cb52b2cbeee1b4f51e338ad3d8644c63d946
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-01-03 15:06:09 +0100

    users service with kafka

--- Users/src/main/scala/users/Kafka.scala
+++ Users/src/main/scala/users/Kafka.scala
@@ -1,9 +1,15 @@
 package users
 
 import scala.concurrent.*
+import scala.jdk.OptionConverters.*
+import scala.jdk.CollectionConverters.*
 import java.util.Properties
 import upickle.default.*
 import org.apache.kafka.clients.producer.*
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.admin.*
+import org.apache.kafka.common.config.TopicConfig
+import org.apache.kafka.common.errors.TopicExistsException
 
 object Kafka {
 
@@ -23,6 +29,30 @@ object Kafka {
   );
 
   val producer = KafkaProducer[String, String](producerConfig);
+
+  def newConsumer: KafkaConsumer[String, String] =
+    val consumerConfig = new Properties();
+    consumerConfig.setProperty(
+      "bootstrap.servers",
+      "users-es:9092"
+    ); // TODO: externalize config
+    consumerConfig.setProperty(
+      "key.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer"
+    );
+    consumerConfig.setProperty(
+      "value.deserializer",
+      "org.apache.kafka.common.serialization.StringDeserializer"
+    );
+    KafkaConsumer[String, String](consumerConfig);
+
+  def withNewConsumer[A](f: KafkaConsumer[String, String] => A): A =
+    val consumer = newConsumer
+    try {
+      f(consumer)
+    } finally {
+      consumer.close()
+    }
 
   def send(
       topic: String,
@@ -58,5 +88,53 @@ object Kafka {
       value: T
   )(using ec: ExecutionContext): Future[RecordMetadata] =
     send(topic, partition, key, write(value))
+
+  enum CleanupPolicy:
+    case Compact
+    case Delete
+
+  case class TopicAlreadyExists()
+
+  case class TopicBuilder(
+      name: String,
+      partitions: Option[Int] = None,
+      replicationFactor: Option[Short] = None,
+      cleanupPolicy: Option[CleanupPolicy] = None
+  )(using ec: ExecutionContext):
+    def create: Future[Either[TopicAlreadyExists, Unit]] =
+      val config = new Properties();
+      config.setProperty(
+        "bootstrap.servers",
+        "users-es:9092"
+      ); // TODO: externalize config
+
+      val admin = Admin.create(config)
+      val topic = NewTopic(
+        name,
+        partitions.map(java.lang.Integer(_)).toJava,
+        replicationFactor.map(java.lang.Short(_)).toJava
+      )
+      val configs = cleanupPolicy match
+        case None => Map()
+        case Some(CleanupPolicy.Compact) =>
+          Map(
+            (TopicConfig.CLEANUP_POLICY_CONFIG -> TopicConfig.CLEANUP_POLICY_COMPACT)
+          )
+        case Some(CleanupPolicy.Delete) =>
+          Map(
+            (TopicConfig.CLEANUP_POLICY_CONFIG -> TopicConfig.CLEANUP_POLICY_DELETE)
+          )
+      topic.configs(configs.asJava)
+
+      Future:
+        val createTopicOperation = admin.createTopics(List(topic).asJava).all()
+
+        try Right(createTopicOperation.get())
+        catch
+          case e
+              if createTopicOperation
+                .exceptionNow()
+                .isInstanceOf[TopicExistsException] =>
+            Left(TopicAlreadyExists())
 
 }
--- Users/src/main/scala/users/domain/UsersServiceImpl.scala
+++ Users/src/main/scala/users/domain/UsersServiceImpl.scala
@@ -1,21 +1,88 @@
 package users.domain;
 
+import scala.concurrent.*
 import users.domain.model.*;
 import users.domain.errors.*
 import users.ports.persistence.UsersRepository;
 
+// TODO: remove
+import upickle.default.*
+import users.Kafka
+import scala.collection.JavaConverters.*
+import users.Kafka.CleanupPolicy
+import org.apache.kafka.common.TopicPartition
+
 class UsersServiceImpl(private val usersRepository: UsersRepository)
     extends UsersService:
+  val topic = "users"
+
+  // TODO: remove
+  given ReadWriter[Username] = ReadWriter.derived
+  given ReadWriter[User] = ReadWriter.derived
 
   override def registerUser(
       username: Username
-  ): Either[UsernameAlreadyInUse, User] =
-    val user = User(username)
-    usersRepository.insert(user.username, user) match
-      case Left(value)  => Left(UsernameAlreadyInUse(username))
-      case Right(value) => Right(user)
+  )(using ec: ExecutionContext): Future[Either[UsernameAlreadyInUse, User]] =
+    // This way when the registerUser future completes it is sure that calls to users() will contain the new user
+    def waitForUserCreation(username: Username): Future[Unit] =
+      for
+        users <- users()
+        _ <-
+          if users.exists(_.username == username) then Future.unit
+          else waitForUserCreation(username)
+      yield ()
 
-  override def users(): Iterable[User] =
-    usersRepository.getAll()
+    val user = User(username)
+    for
+      users <- users() // creates the topic if not exist
+      res <-
+        if users.exists(_ == user) then
+          Future(Left(UsernameAlreadyInUse(username)))
+        else
+          Kafka.send("users", username.value, user)
+          waitForUserCreation(username).map(_ => Right(user))
+    yield (res)
+
+  override def users()(using ec: ExecutionContext): Future[Iterable[User]] =
+    for
+      _ <- createTopicIfNotExist()
+      res <-
+        Future:
+          Kafka.withNewConsumer: consumer =>
+            consumer.assign(List(TopicPartition(topic, 0)).asJava)
+            consumer.seekToBeginning(List().asJava)
+            Iterator
+              .continually(
+                consumer.poll(java.time.Duration.ofMillis(20)).asScala
+              )
+              .takeWhile(_.nonEmpty)
+              .flatten
+              .map(r => (Username(r.key()), read[User](r.value())))
+              .toMap
+              .values
+    yield (res)
+
+  private var _topicCreated = false
+  private def topicCreated: Boolean = synchronized(_topicCreated)
+  private def topicCreated_=(newValue: Boolean): Unit =
+    synchronized:
+      _topicCreated = newValue
+
+  private def createTopicIfNotExist()(using
+      ec: ExecutionContext
+  ): Future[Unit] =
+    if topicCreated then Future.unit
+    else
+      Kafka
+        .TopicBuilder(
+          "users",
+          partitions = Some(1),
+          cleanupPolicy = Some(CleanupPolicy.Compact)
+        )
+        .create
+        .map(_ match
+          case Left(_)  => topicCreated = true
+          case Right(_) => ()
+        )
 
   override def healthCheckError(): Option[String] = None
