12b52b7d11b577c2ca77801f14abad18331960c2
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-01-31 09:24:46 +0100

    Removed unused classes

--- Rides/src/main/scala/rides/ports/persistence/RidesRepository.scala
+++
@@ -1,7 +0,0 @@
-package rides.ports.persistence;
-
-import rides.domain.model.*
-import shared.ports.persistence.Repository;
-import shared.ports.persistence.exceptions.*;
-
-trait RidesRepository extends Repository[RideId, Ride]
